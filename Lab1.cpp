880da0d214c6dd7021c1b09252e45fd76148bfda
Gatalski <66117686+GatalskiAnton@users.noreply.github.com>
2021-09-16 20:39:44 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -6,32 +6,32 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
+	
 	int K;
 	int c;
-	cout << "Введите количество цифр после запятой\n";
+	cout << "Enter the number of digits after the decimal point\n";
 	cin >> c;
-	cout << "Введите K\n";
+	cout << "Enter K\n";
 	cin >> K;
-	//cout « "Введите E\n";
-	long double e;
-	//cin » e;
-	e = (pow(10, -K) / 2);
-	cout << "Введите X∈(-1;1)\n";
+	long double epsilon;
+	epsilon = (pow(10, -K) / 2);
+	cout << "Enter X∈(-1;1)\n";
 	long double x;
 	cin >> x;
-	long double res_1;
-	res_1 = log((1 + x) / (1 - x));
-	long double res_2 = 0;
+	long double math_h_result;
+	math_h_result = log((1 + x) / (1 - x));
+	long double taylor_result = 0;
 	float i = 1;
 
-	while (var < e)
+	while (step < epsilon)
 	{
 
 
-		float var;
-		var = (pow(x, i) / i);		
-		res_2 = res_2 + var;
+		float step;
+		step = (pow(x, i) / i);
+
+		
+		taelor_res = taylor_res + step;
 		i += 2;
 
 
@@ -39,6 +39,6 @@ int main()
 
 
 	res_2 *= 2;
-	cout << "Используя math.h: " << setprecision(c + 1) << res_1 << endl;
-	cout << "Использую ряд Тейлора: " << setprecision(c + 1) << res_2;
+	cout << "Используя math.h: " << setprecision(c + 1) << math_h_result << endl;
+	cout << "Использую ряд Тейлора: " << setprecision(c + 1) << taylor_result;
 }
